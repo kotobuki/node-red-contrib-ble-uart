f748dc5a8c8bfaf619e624250d4ccb33dde61765
Shigeru Kobayashi <kotobuki@gmail.com>
2016-08-04 11:40:37 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # node-red-contrib-ble-uart
-A Node-RED node that uses the Noble to interact with BLE devices that support the UART profile by Nordic Semiconductor
+A [Node-RED](http://nodered.org/) node that uses the Noble to interact with BLE devices that support the UART profile by Nordic Semiconductor
+
+## Prerequisites
+
+* [noble](https://github.com/sandeepmistry/noble), a Node.js BLE (Bluetooth Low Energy) central module, by Sandeep Mistry
+
+## Install
+
+```sh
+npm install node-red-contrib-ble-uart
+```
+
+## Example
+
+```json
+[{"id":"99af4ec8.c6d5a","type":"debug","z":"70817eb5.feb8f","name":"","active":true,"console":"false","complete":"false","x":630,"y":60,"wires":[]},{"id":"dfd9a66b.1bfc78","type":"inject","z":"70817eb5.feb8f","name":"On","topic":"","payload":"On*","payloadType":"str","repeat":"","crontab":"","once":false,"x":90,"y":40,"wires":[["43a2b84.776b048"]]},{"id":"88e4011b.91e15","type":"inject","z":"70817eb5.feb8f","name":"Off","topic":"","payload":"Off*","payloadType":"str","repeat":"","crontab":"","once":false,"x":90,"y":80,"wires":[["43a2b84.776b048"]]},{"id":"6bfdc8db.8b2f78","type":"function","z":"70817eb5.feb8f","name":"To String","func":"msg.payload = String(msg.payload);\n\nreturn msg;","outputs":1,"noerr":0,"x":440,"y":60,"wires":[["99af4ec8.c6d5a"]]},{"id":"43a2b84.776b048","type":"ble uart","z":"70817eb5.feb8f","name":"","localName":"Adafruit Bluefruit LE","x":260,"y":60,"wires":[["6bfdc8db.8b2f78"]]}]
+```
---
+++ ble-uart.js
@@ -0,0 +1,139 @@
+module.exports = function(RED) {
+    var noble = require('noble');
+
+    function BleUartNode(config) {
+        RED.nodes.createNode(this, config);
+
+        // The proprietary UART profile by Nordic Semiconductor
+        var uartServiceUuid = '6e400001b5a3f393e0a9e50e24dcca9e';
+        var txCharacteristicUuid = '6e400002b5a3f393e0a9e50e24dcca9e';
+        var rxCharacteristicUuid = '6e400003b5a3f393e0a9e50e24dcca9e';
+
+        var txCharacteristic, rxCharacteristic;
+        var writeWithoutResponse = false;
+
+        var node = this;
+        var localName = config.localName;
+        if (localName === "") {
+            node.error("Please specify a local name");
+        }
+
+        this.on('input', function(msg) {
+            if (txCharacteristic) {
+                // Oou can only send at most 20 bytes in a Bluetooth LE pacet,
+                // so slice the data into 20-byte chunks:
+                while (msg.payload.length > 20) {
+                    var output = msg.payload.slice(0, 19);
+                    txCharacteristic.write(new Buffer(output), writeWithoutResponse);
+                    msg.payload = data.slice(20);
+                }
+
+                // Send any remainder bytes less than the last 20:
+                txCharacteristic.write(new Buffer(msg.payload), writeWithoutResponse);
+            }
+        });
+
+        noble.on('stateChange', function(state) {
+            if (state === 'poweredOn') {
+                noble.startScanning([uartServiceUuid], false);
+                node.status({ fill: "blue", shape: "ring", text: "BLE powered on" });
+            } else {
+                noble.stopScanning();
+                node.status({ fill: "red", shape: "ring", text: "BLE powered off" });
+            }
+        });
+
+        noble.on('scanStart', function() {
+            node.log('Started scanning');
+            node.status({ fill: "blue", shape: "ring", text: "Scanning" });
+        });
+
+        noble.on('scanStop', function() {
+            node.log('Stopped scanning');
+            node.status({ fill: "blue", shape: "dot", text: "Scanning finished" });
+        });
+
+        noble.on('warning', function(message) {
+            node.warn(message);
+        });
+
+        noble.on('discover', function(peripheral) {
+            node.log('Discovered: ' + peripheral);
+
+            if (peripheral.advertisement.localName !== localName) {
+                // Ignore non-specified peripherals
+                return;
+            }
+
+            // We found a peripheral, stop scanning
+            noble.stopScanning();
+            node.log('Found: ' + peripheral.advertisement.localName);
+            node.status({ fill: "green", shape: "ring", text: "Device found" });
+
+            peripheral.connect(function(err) {
+                if (err !== undefined) {
+                    node.error('Error connecting: ' + err);
+                    return;
+                }
+
+                node.log('Connected to ' + peripheral.advertisement.localName);
+                node.status({ fill: "green", shape: "ring", text: "Device connected" });
+
+                peripheral.discoverServices(null, function(err, services) {
+                    if (err === undefined) {
+                        node.error('Error discovering services: ' + err);
+                    }
+
+                    node.log('Services: ' + services.length);
+                    services.forEach(function(service) {
+                        if (service.uuid !== uartServiceUuid) {
+                            // Ignore non-UART services
+                            return;
+                        } else {
+                            node.log('Found a UART service');
+                        }
+
+                        service.discoverCharacteristics(null, function(err, characteristics) {
+                            characteristics.forEach(function(characteristic) {
+                                if (txCharacteristicUuid == characteristic.uuid) {
+                                    txCharacteristic = characteristic;
+
+                                    if (characteristic.properties.indexOf("writeWithoutResponse") > -1) {
+                                        writeWithoutResponse = true;
+                                    }
+                                    node.log('writeWithoutResponse: ' + writeWithoutResponse);
+
+                                    node.log('Found a TX characteristic');
+                                } else if (rxCharacteristicUuid == characteristic.uuid) {
+                                    rxCharacteristic = characteristic;
+                                    node.log('Found a RX characteristic');
+
+                                    // Turn on notifications
+                                    rxCharacteristic.notify(true);
+
+                                    rxCharacteristic.on('read', function(data, notification) {
+                                        node.log(data);
+
+                                        // If you got a notification
+                                        if (notification) {
+                                            // var msg = { "payload": String(data) };
+                                            var msg = { "payload": data };
+                                            node.send(msg);
+                                        }
+                                    });
+                                }
+
+                                if (txCharacteristic && rxCharacteristic) {
+                                    node.log('Ready');
+                                    node.status({ fill: "green", shape: "dot", text: "Device ready" });
+                                }
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    }
+
+    RED.nodes.registerType("ble uart", BleUartNode);
+}
